1a4bba4842b10fee2b852349501b2394fc5a65a7
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2015-05-05 01:58:47 +0200

    change port to 5000

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var config = {
   entry: {
     app: [
-      'webpack-dev-server/client?http://localhost:8080',
+      'webpack-dev-server/client?http://localhost:5000',
       'webpack/hot/only-dev-server',
       './src/entry.jsx'
     ],
