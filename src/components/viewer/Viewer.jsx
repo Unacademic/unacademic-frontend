5d45ad3badcc0563832fadc438c412dc49d394e5
gauthiier <d@gauthiier.info>
2015-05-21 17:22:11 +0200

    view enable iframe

--- src/components/viewer/Viewer.jsx
+++ src/components/viewer/Viewer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-class Viewer extends React.Component {
+class Viewer extends React.Component {	
+
   render() {
     //let data = this.props.data
     let html_content = this.parse(this.props.data)
@@ -13,15 +14,24 @@ class Viewer extends React.Component {
   }
 
   parse(data) {
+
+  	// read from config file? perhaps better than declaring here!
+  	let iframe_able = true;
+
   	if(data.content === null)
   		if(data.media.hasOwnProperty('html')) {
   			return data.media.html;  			
   		}
   		else {
-  			let warning = '<warning>The current resource could not be loaded...<br>'
-  			warning += '<img src="https://nyobetabeat.files.wordpress.com/2012/06/anigif_777-2432-1303312751-40.gif"/>'
-  			warning += '<br><br>Please refer to the following link:\n'
-  			return warning + '<div><a href=' + data.url + ' target="_blank">' + data.title + '</a></div></warning>'  			
+  			let https = 'https'
+  			if(data.url.substring(0, https.length) === https || !iframe_able) {
+	  			let warning = '<warning>The current resource could not be loaded...<br>'
+	  			warning += '<img src="https://nyobetabeat.files.wordpress.com/2012/06/anigif_777-2432-1303312751-40.gif"/>'
+	  			warning += '<br><br>Please refer to the following link:\n'
+	  			return warning + '<div><a href=' + data.url + ' target="_blank">' + data.title + '</a></div></warning>'  			  				
+  			} else {
+  				return '<iframe frameborder="0" src=' + data.url + '>' + data.url + '</iframe>'
+  			}
   		}
   	return data.content
   }
