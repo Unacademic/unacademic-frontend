68de6b0e14c7fc3ecd3f2e62deb65913bd3538b3
Greg RV <gregvarias@gmail.com>
2015-05-25 10:53:06 +0200

    Added introjs tour with styling.

--- src/components/app/Unacademic.jsx
+++ src/components/app/Unacademic.jsx
@@ -2,26 +2,51 @@ import React from 'react';
 import Sidebar from '../sidebar/Sidebar.jsx';
 import Main from '../main/Main.jsx';
 
-class Unacademic extends React.Component{
-  render() {
+var TourGuideMixin = require('react-tour-guide').Mixin;
+
+var tour = {
+  startIndex: 0,
+  scrollToSteps: true,
+  steps: [
+    {
+      text: 'Explore a Waypoint- our construct for guided learning!',
+      element:  '.btn',
+      position: 'bottom-left'
+    },
+    {
+      text: "Here's the Waypoint you just clicked. Here in the Sidebar, you'll always find relevant information for the current level you're on!",
+      element: '.title',
+      position: 'bottom-right'
+    },
+    {
+      text: "A Waypoint is comprised of Checkpoints, made to break the learning into focused, digestable sections.",
+      element: '.btn',
+      position: 'left'
+    }
+  ]
+};
+var cb = function() {
+  console.log('User has completed tour!');
+};
+
+var Unacademic = React.createClass({
+
+  mixins: [TourGuideMixin(tour, cb)],
+
+  render: function(){
     let { appState, viewModel } = this.props;
     let currentLevel = appState.levels.current;
     let currentMode = appState.modes.current;
     let { model, collection, url, data } = viewModel;
     let classes = `layout-app layout-app-is-${currentLevel} layout-app-is-${currentMode}`;
 
-    return (
-      <section className={ classes }>
-        <Sidebar model={ model } appState={ appState }/>
-        <Main levels={ appState.levels } collection={ collection } data={ data } />
-      </section>
-    )
-  }
-};
+      return (
+        <section className={ classes }>
+          <Sidebar model={ model } appState={ appState }/>
+          <Main levels={ appState.levels } collection={ collection } data={ data } />
+        </section>
+      )
+    }
+});
 
-Unacademic.propTypes = {
-  appState: React.PropTypes.object.isRequired,
-  viewModel: React.PropTypes.object
-}
-
-export default Unacademic;
+module.exports = Unacademic;
