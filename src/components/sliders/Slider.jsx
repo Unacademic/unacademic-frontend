6f7f92e5b53dd18e20505ced0afb999b3e76966f
Greg RV <gregvarias@gmail.com>
2015-05-27 02:15:12 +0200

    Add sliderinfo to Slider component to neatly display slider labels and provide relevant descriptions of each field on hover.

--- src/components/sliders/Slider.jsx
+++ src/components/sliders/Slider.jsx
@@ -10,23 +10,37 @@ class Slider extends React.Component {
 
   render() {
     let { fieldName, value } = this.props;
-    let title = {
-      clarity: "How clearly does this resource explain the subject?",
-      difficulty: "How difficult is this resource to understand?",
-      up_to_date: "How up-to-date is this resource with latest developments in the field?",
-      pertinence: "How relevant is this resource to its Checkpoint?",
-      enjoyment: "How engaging and fun is this resource?",
+    let sliderInfo = {
+      clarity: {
+        properLabel: "Clarity",
+        title: "How clearly does this resource explain the subject?"
+      },
+      difficulty: {
+        properLabel: "Difficulty",
+        title: "How difficult is this resource to understand?"
+      },
+      up_to_date: {
+        properLabel: "Up to Date",
+        title: "How up-to-date is this resource with latest developments in the field?"
+      },
+      pertinence: {
+        properLabel: "Pertinence",
+        title: "How relevant is this resource to its Checkpoint?"
+      },
+      enjoyment: {
+        properLabel: "Enjoyment",
+        title: "How engaging and fun is this resource?"
+      }
     };
 
     return (
       <div key={ fieldName } className="slider">
-      <label htmlFor={ fieldName } title={ title[fieldName] }>{ fieldName }</label>
+      <label htmlFor={ fieldName } title={ sliderInfo[fieldName].title }>{ sliderInfo[fieldName].properLabel }</label>
       <input type="range" name={ fieldName } min="0" max="5" step="1"
       value={ value }
       onChange={ this.handleChange.bind(this) } /><br />
       </div>
-    )
-
+    );
   }
 }
 
